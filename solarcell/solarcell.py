e357eb82994ec97068eaa1d86268987ef8ad2b6a
amosborne <amosborne@users.noreply.github.com>
2024-01-07 07:25:11 -0800

    wip

--- solarcell/solarcell.py
+++ solarcell/solarcell.py
@@ -3,9 +3,10 @@ from inspect import Signature
 
 import numpy as np
 from scipy import constants
-from scipy.optimize import brute, root_scalar, minimize
+from scipy.optimize import brute, minimize, root_scalar, least_squares
 from scipy.special import lambertw
 
+from warnings import warn
 
 class _metrics:
     def __init__(self, isc, voc, imp, vmp, iv, vi):
@@ -48,7 +49,7 @@ def _model(iph, i0, rs, rsh, n, t, nsamp):
     nvth = n * constants.k * (t + 273.15) / constants.e
     rtot = rs + rsh
     itot = iph + i0
-    
+
     @np.errstate(over="ignore")
     def iv(v):
         # equation #2 from the technical reference
@@ -79,41 +80,30 @@ def _model(iph, i0, rs, rsh, n, t, nsamp):
     return isc, voc, imp, vmp, iv, vi
 
 
-def _fit(x, isc, voc, imp, vmp, t, nsamp):
-    return _model(
-        iph=isc * x[0],
-        i0=isc * 10**x[1],
-        rs=(voc - vmp) / imp * x[2],
-        rsh=vmp / (isc - imp) * x[3],
-        n=x[4],
-        t=t,
-        nsamp=nsamp,
-    )
-
-def _cost(x, isc, voc, imp, vmp, wi, wv, t, nsamp, rss=False):
-    xisc, xvoc, ximp, xvmp, _, _ = _fit(x, isc, voc, imp, vmp, t, nsamp)
-    actual = (xisc * wi, xvoc * wv, ximp * wi, xvmp * wv)
-    target = (isc * wi, voc * wv, imp * wi, vmp * wv)
-    errors = np.subtract(target, actual)
-    errors = np.nan_to_num(errors, nan=1e16, posinf=1e16, neginf=-1e16)
-    abserr = np.sum(errors**2)
-    relerr = np.sqrt(np.sum(np.divide(errors, target)**2))
-    return relerr if rss else abserr
-
-
 class solarcell:
-    rss = 0.01  # the RSS error threshold at which the fit fails
+    rss = 0.2  # the RSS error threshold at which the fit fails
     nsamp = 1000  # number of samples used to interpolate curves
 
-    def __init__(self, isc, voc, imp, vmp, t):
-        assert isc[0] > imp[0], "Isc must exceed Imp."
+    def __init__(self, jsc, voc, jmp, vmp, area, rs=1, t=28):
+        # jsc: (mA/cm2, uA/cm2/K), short circuit current density
+        # voc: (V, mV/K), open circuit voltage
+        # jmp: (mA/cm2, uA/cm2/K), max power point current density
+        # vmp: (V, mV/K), max power point voltage
+        # rs: ohm*cm2, area-normalized series resistance
+        # t: C, reference temperature
+        
+        assert jsc[0] > jmp[0], "Isc must exceed Imp."
         assert voc[0] > vmp[0], "Voc must exceed Vmp."
         assert t > -273.15, "Temperature must exceed absolute zero."
-        self.isc = isc
-        self.voc = voc
-        self.imp = imp
-        self.vmp = vmp
-        self.t = t
+        assert 0.5 <= rs <= 1.3, "Rs is outside the typical range of 0.5 <= rs <= 1.3 ohm*cm2."
+        
+        self.isc = (jsc[0] * 1e-3 * area, jsc[1] * 1e-6 * area) # (A, A/K)
+        self.voc = (voc[0], voc[1] * 1e-3) # (V, V/K)
+        self.imp = (jmp[0] * 1e-3 * area, jmp[1] * 1e-6 * area) # (A, A/K)
+        self.vmp = (vmp[0], vmp[1] * 1e-3) # (V, V/K)
+        self.rs = rs / area # ohm
+        self.area = area # cm2
+        self.t = t # C
 
     @_round_cell_inputs(ttol=0.1, gtol=0.01)
     @lru_cache(maxsize=128)
@@ -137,41 +127,46 @@ class solarcell:
         assert isc > imp, "Isc must exceed Imp."
         assert voc > vmp, "Voc must exceed Vmp."
 
-        # Perform the fit by a recursive brute-force method.      
-        ns = (7, 7, 7, 7, 7)
-        args = (isc, voc, imp, vmp, wi, wv, t, self.nsamp)
+        # Use least-squares numeric optimized to solve for (i0, rs, rsh, n).
+        def model(x):
+            return _model(isc, 10**x[0], 1 / self.area, 10**x[1], x[2], t, self.nsamp)
 
-        ub = (1.10, -16, 0.100, 1000, 6.4)
-        lb = (1.00, -22, 0.001,   10, 0.4)
+        def resids(x, rss=False):
+            xisc, xvoc, ximp, xvmp, _, _ = model(x)
+            actual = (xisc * wi, xvoc * wv, ximp * wi, xvmp * wv)
+            target = (isc * wi, voc * wv, imp * wi, vmp * wv)
+
+            if rss:
+                print("Fit Targets:", _metrics(*target, None, None))
+                print("Fit Results:", _metrics(*actual, None, None))
+            
+            errors = np.subtract(target, actual)
+            errors = np.nan_to_num(errors, nan=1e16, posinf=1e16, neginf=-1e16)
+            relerr = np.sqrt(np.sum(np.divide(errors, target) ** 2))
+            return relerr if rss else errors
+
+        cost = lambda x: np.sum(resids(x)**2)
+        lb = (-24, np.log10(1e3 / self.area), 0.4)
+        ub = (-10, np.log10(1e6 / self.area), 8.0)
+        x = brute(cost, tuple(zip(lb, ub)), Ns=24, finish=None)
+
+        # lb = (-24, 0.5 / self.area, np.log10(1e3 / self.area), 0.4)
+        # ub = (-10, 1.3 / self.area, np.log10(1e6 / self.area), 8.0)
+        # x0 = (-18, 1.0 / self.area, np.log10(1e4 / self.area), 2.0)
+        # result = least_squares(resids, x0, bounds=(lb, ub))
+        # print(result)
+        # assert result.success
+
         
-        ranges = [slice(lx, ux, complex(nx)) for lx, ux, nx in zip(lb, ub, ns)]
-        x0 = brute(_cost, ranges, args, finish=None, workers=-1)
-        print("Fit UB:", ("{:6.2f} " * 5).format(*ub))
-        print("Fit X0:", ("{:6.2f} " * 5).format(*x0))
-        print("Fit LB:", ("{:6.2f} " * 5).format(*lb))
+        print("Fit Solution:", x)
 
-        step = [(ux - lx) / (nx - 1) for lx, ux, nx in zip(lb, ub, ns)]
-        lb = [max(xy - stepy, lx) for xy, stepy, lx in zip(x0, step, lb)]
-        ub = [min(xy + stepy, ux) for xy, stepy, ux in zip(x0, step, ub)]
-        ranges = [slice(lx, ux, complex(nx)) for lx, ux, nx in zip(lb, ub, ns)]
-        x0 = brute(_cost, ranges, args, finish=None, workers=-1)
-        print("Fit UB:", ("{:6.2f} " * 5).format(*ub))
-        print("Fit X0:", ("{:6.2f} " * 5).format(*x0))
-        print("Fit LB:", ("{:6.2f} " * 5).format(*lb))
+        rss = resids(x, rss=True)
+        print("Fit RSS:", rss)
         
-        xisc, xvoc, ximp, xvmp, _, _ = _fit(x0, isc, voc, imp, vmp, t, self.nsamp)
-        actual = (xisc * wi, xvoc * wv, ximp * wi, xvmp * wv)
-        target = (isc * wi, voc * wv, imp * wi, vmp * wv)
-
-        print("Fit Targets:", _metrics(*target, None, None))
-        print("Fit Results:", _metrics(*actual, None, None))
-        print("Fit RSS:", _cost(x0, *args, rss=True))
-
-        rss = _cost(x0, *args, rss=True)
         emsg = "Failed model fit. RSS error of {:0.1f}% exceeds {:0.1f}%."
-        # assert rss < self.rss, emsg.format(rss * 100, self.rss * 100)
+        assert rss < self.rss, emsg.format(rss * 100, self.rss * 100)
 
-        return _metrics(*_fit(x0, isc, voc, imp, vmp, t, self.nsamp))
+        return _metrics(*model(x))
 
     def string(self, t, g, wi):
         # All series cells in a string have equal current.
